217e531fc9ed23d4f0ffdba23811c6f8d20dda17
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-24 09:50:53 +0700

    menambah box

--- app/src/main/java/com/example/project02/UIPertama.kt
+++ app/src/main/java/com/example/project02/UIPertama.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -20,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -81,6 +83,8 @@ fun ActivitasPertama(modifier: Modifier){
         }
         Box(
             modifier = Modifier
+                .fillMaxSize()
+
     }
 
 
