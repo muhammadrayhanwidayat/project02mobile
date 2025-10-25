2d08d4afd87090774a0de268f9dab2e92c2c08af
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-25 17:08:34 +0700

    foto profile

--- app/src/main/java/com/example/project02/Profile.kt
+++ app/src/main/java/com/example/project02/Profile.kt
@@ -1,5 +1,15 @@
 package com.example.project02
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun Profile(modifier: Modifier = Modifier) {
@@ -10,3 +20,12 @@ fun Profile(modifier: Modifier = Modifier) {
             .padding(top = 100.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Image(
+            painter = painterResource(id = R.drawable.saya),
+            contentDescription = "Foto Profil",
+            modifier = Modifier
+                .size(140.dp)
+                .clip(CircleShape)
+                .border(3.dp, Color.Gray, CircleShape),
+            contentScale = ContentScale.Crop
+        )
