d3292742c70d2f853cd919b74a9b09ce46477ecb
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-25 17:19:56 +0700

    menambah dependensi

--- app/src/main/java/com/example/project02/Profile.kt
+++ app/src/main/java/com/example/project02/Profile.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
