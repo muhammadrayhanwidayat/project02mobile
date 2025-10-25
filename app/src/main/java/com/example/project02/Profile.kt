f6fe8347c7bd27c8402c252dabc51e1f2088c661
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-25 17:07:09 +0700

    menambah profile

---
+++ app/src/main/java/com/example/project02/Profile.kt
@@ -0,0 +1,12 @@
+package com.example.project02
+
+
+@Composable
+fun Profile(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(top = 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
