804bf74bc50deebc9d036da23f8f9b80ec3a467b
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-25 17:19:13 +0700

    menambah box

--- app/src/main/java/com/example/project02/Profile.kt
+++ app/src/main/java/com/example/project02/Profile.kt
@@ -247,3 +247,18 @@ fun Profile(modifier: Modifier = Modifier) {
                     )
                 }
             }
+
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.copy),
+                    color = Color.White,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = 50.dp)
+                )
+            }
+        }
+    }
+}
